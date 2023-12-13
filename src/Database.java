35388e5ed4f638a60cfe3ca0bae454c93bcdc8b3
K0nny16 <kowal.karl99@gmail.com>
2023-12-13 15:25:30 +0100

    GUI

---
+++ src/Database.java
@@ -0,0 +1,55 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    private final String baseURL = "https://api-och-firebase-default-rtdb.europe-west1.firebasedatabase.app/";
+    static List<String[]> data;
+    Database(){
+        getRequest();
+    }
+    void getRequest(){
+        try{
+            URL url = new URL(baseURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while ((line = reader.readLine()) != null )
+                    response.append(line);
+                reader.close();
+                JSON(response);
+            }
+            else
+                System.out.println("Error response code: "+responseCode);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    void JSON (StringBuilder string) throws Exception {                     
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(String.valueOf(string));
+        JSONArray jsonArray = (JSONArray) obj;
+
+        for (Object json : jsonArray) {
+            JSONObject jsonObject = (JSONObject) json;
+            List<String> rowData = new ArrayList<>();
+
+            for (Object key : jsonObject.keySet())
+                rowData.add(jsonObject.get(key).toString());
+            data.add(rowData.toArray(new String[0]));
+        }
+    }
+    static String[] getNames(){
+        return data.getFirst();
+    }
+}
+
--- src/GUI.java
+++ src/GUI.java
@@ -1,19 +1,51 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.util.Arrays;
 
 class GUI extends JFrame {
+    JPanel center;
     GUI(){
+        new Database();
         setLayout(new BorderLayout());
         JPanel top = new JPanel();
-        add(top,BorderLayout.NORTH);
-        JPanel center = new JPanel();
-        add(center,BorderLayout.CENTER);
+        top.setLayout(new BoxLayout(top,BoxLayout.X_AXIS));
+        center = new JPanel();
+        Border gray = BorderFactory.createLineBorder(Color.GRAY);
         center.setLayout(new GridLayout(0,4,0,1));
 
+        addUser(top);
+        addContent(gray);
+        selectUser(top);
 
+        add(top,BorderLayout.NORTH);
+        add(center,BorderLayout.CENTER);
         setTitle("Weather App");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(400,400);
         setVisible(true);
+    }
+    void addContent(Border border){
+        for(int i = 0; i < 4; i++){
+            JPanel panel = new JPanel();
+            panel.setBorder(border);
+            center.add(panel);
+        }
+    }
+    void addUser(JPanel panel){
+        JButton addUser = new JButton("Add User");
+        addUser.addActionListener(e -> userMenu());
+        panel.add(addUser);
+    }
+    void selectUser(JPanel panel){
+        JComboBox<String> users = new JComboBox<>(Database.getNames());
+
+        panel.add(users);
+    }
+    void userMenu(){
+        String username = JOptionPane.showInputDialog("Enter your profile name:");
+        String unFormatted = JOptionPane.showInputDialog("Which cities do you want the weather of?(ex Malmö,Trelleborg,Svedala,Ystad)");
+        String[] formatted = unFormatted.split(",");
+        System.out.println(Arrays.toString(formatted));
     }
 }
