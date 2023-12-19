cdd987d4fe0cb5af9a526b494551c7232eec324c
Karl Kowal <kowal.karl99@gmail.com>
2023-12-19 22:11:25 +0100

    V1.0
    Visar all data med bild. Förbättringar är att displaya det snyggare och göra kod förbättringar.

--- src/GUI.java
+++ src/GUI.java
@@ -1,56 +1,76 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
-import javax.xml.crypto.Data;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.util.Arrays;
 class GUI extends JFrame {
-    static private String selectedUser;
-    static private String selectLocations;
     GUI(){
         setLayout(new BorderLayout());
         JPanel top = new JPanel();
         top.setLayout(new FlowLayout());
-        JPanel center = new JPanel();
-        Border gray = BorderFactory.createLineBorder(Color.GRAY);
-        center.setLayout(new BorderLayout());
-
         selectCity(top);
-        addContent(gray,center);
-
         add(top,BorderLayout.NORTH);
-        add(center,BorderLayout.CENTER);
         setTitle("Weather App");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(400,400);
+        setSize(800,800);
+        setLocationRelativeTo(null);
         setVisible(true);
     }
-    void addContent(Border border,JPanel center){
-       JPanel panel = new JPanel();
-       panel.setBorder(border);
+    void addContent(String[] data){
+        Border gray = BorderFactory.createLineBorder(Color.GRAY);
+        JPanel center = new JPanel();
+        center.setLayout(new BorderLayout());
+        center.setBorder(gray);
 
-       //Väder
+        try{
+            BufferedImage image = ImageIO.read(new URL("https://openweathermap.org/img/wn/"+data[5]+"@2x.png"));
+            if(image != null){
+                Image resizedImg = image.getScaledInstance(120,120,Image.SCALE_SMOOTH);
+                JLabel pic = new JLabel(new ImageIcon(resizedImg));
+                JPanel textPanel = new JPanel();
 
-       center.add(panel);
+                textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+                for(int i = 0; i < data.length-1; i++){
+                    JLabel text = new JLabel(data[i]);
+                    text.setFont(new Font("Arial",Font.BOLD,16));
+                    textPanel.add(text);
+                }
+                center.add(textPanel, BorderLayout.NORTH);
+                center.add(pic,BorderLayout.CENTER);
+                add(center,BorderLayout.CENTER);
+                revalidate();
+            }
+            else {
+                System.out.println("Error");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
     void selectCity(JPanel panel){
         JButton addUser = new JButton("Pick city!");
         addUser.addActionListener(e -> {
             try {
-                addMenu();
+                addCity();
             } catch (MalformedURLException ex) {
                 throw new RuntimeException(ex);
             }
         });
+        addUser.setFocusable(false);
         panel.add(addUser);
     }
-    void addMenu() throws MalformedURLException {
+    void addCity() throws MalformedURLException {
         String city = JOptionPane.showInputDialog("What city do you want the weather of? \n ex Malmö,Trelleborg,Svedala,Ystad");
         if (city != null && !city.isEmpty()){
             String response = WeatherAPI.getLL(new URL("http://api.openweathermap.org/geo/1.0/direct?q="+city+",se&limit=1&appid=6e86d40c1ccec69010c71630afb27d8c"));
             double[] coords = WeatherAPI.geoJson(response);
-            String weatherResponse = WeatherAPI.getLL(new URL("https://api.openweathermap.org/data/3.0/onecall?lat="+coords[0]+"&lon="+coords[1]+"&exclude=&appid=6e86d40c1ccec69010c71630afb27d8c"));
+            String weatherResponse = WeatherAPI.getLL(new URL("https://api.openweathermap.org/data/2.5/weather?lat="+coords[0]+"&lon="+coords[1]+"&appid=6e86d40c1ccec69010c71630afb27d8c&units=metric&lang=sv"));
+            String[] weatherData = WeatherAPI.weatherJson(weatherResponse);
+            System.out.println(Arrays.toString(weatherData));
+            addContent(weatherData);
         }
         else
             JOptionPane.showMessageDialog(null,"Error!");
--- src/WeatherAPI.java
+++ src/WeatherAPI.java
@@ -1,43 +1,70 @@
 import com.google.gson.*;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 class WeatherAPI {
-   static String getLL(URL url){
-       try{
-           HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-           connection.setRequestMethod("GET");
-           int responseCode = connection.getResponseCode();
-           if(responseCode == HttpURLConnection.HTTP_OK){
-               BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-               String line;
-               StringBuilder response = new StringBuilder();
-               while((line = reader.readLine()) != null)
-                   response.append(line);
-               reader.close();
-               return response.toString();
-           }
-           else
-               System.out.println("Error response code: "+responseCode);
-           connection.disconnect();
-       }catch (Exception e){
-           e.printStackTrace();
-       }
-       return "";
-   }
+    static String getLL(URL url){
+        try{
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+                while((line = reader.readLine()) != null)
+                    response.append(line);
+                reader.close();
+                return response.toString();
+            }
+            else
+                System.out.println("Error response code: "+responseCode);
+            connection.disconnect();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
     static double[] geoJson(String jsonString) {
         GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
         Gson gson = builder.create();
-        City city = gson.fromJson(jsonString.substring(1,jsonString.length()-1), City.class);
+        City city = gson.fromJson(jsonString.substring(1,jsonString.length()-1),City.class);
         return new double[]{city.lat, city.lon};
+    }
+    static String[] weatherJson(String jsonString) {
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();                                                                      //Gör det lättare att se och tolka Json filen ifall den skrivs ut i consolen.
+        Gson gson = builder.create();
+        WeatherData weatherData = gson.fromJson(jsonString, WeatherData.class);                          //Parsar json stringen till objektet av klassen WeatherData.
+        WeatherInfo weather = weatherData.weather[0];                                                   //Hämtar ut det första elementet i arrayen.
+        return new String[]{weatherData.name,                                                           //index 0 namn på stad
+                weatherData.weather[0].description.substring(0,1).toUpperCase()+weatherData.weather[0].description.substring(1), //index 1 beskrivning av väder.
+                weatherData.main.temp +" degrees celsius",                                               //Index2 Tempratur
+                "Feels like: "+ weatherData.main.feels_like +" degrees celsius",                         //Index3 Vilken tempratur det känns som.
+                "Windspeed:"+weatherData.wind.speed+"M/S",                                               //Index4 Vindhastighet.
+                weather.icon};                                                                           //Index5 Icon för import.
+
     }
 }
 class City{
-    String name;
-    double lon;
-    double lat;
+    double lat;         //Latitud.
+    double lon;         //Longitud.
+}
+class WeatherData {
+    WeatherInfo[] weather;
+    Temperatur main;          //Matchar med det i WeatherData som har samma namn (exempel).
+    String name;             //Kan se denna som key / value ? (Objektet = key / datan i den = value).
+    Wind wind;
+}
+class WeatherInfo {
+    String description; //Beskrivning.
+    String icon;        //Icon.
+}
+class Temperatur {
+    double temp;        //Grader.
+    double feels_like;  //Vad det känns som.
+}
+class Wind{
+    double speed;      //Vind hastighet.
 }
